a366e220930a0f14b843ee6b52c30bc1ef4dfcb9
Ramazan HABER <ramazanhaberr@gmail.com>
2023-01-20 19:13:56 +0300

    Add project files.

---
+++ SqlAutoStartR/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace SqlAutoStartR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnKapat = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 15000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(196, 145);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 145);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kaç kere durmuş";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnKapat});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(126, 30);
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.Image = global::SqlAutoStartR.Properties.Resources.exit;
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(125, 26);
+            this.btnKapat.Text = "KAPAT";
+            this.btnKapat.Click += new System.EventHandler(this.btnKapat_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(317, 103);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "BU PROGRAM SQL DURURSA OTOMATİK BAŞLATIR\r\nÇALIŞAN KOMUT İSE \r\n\r\n\"net start mssqls" +
+    "erver\"";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 220);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SQL AUTO START v0.1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem btnKapat;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SqlAutoStartR/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlAutoStartR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SqlStart();
+        }
+
+        public void SqlStart()
+        {
+
+            try
+            {
+                ServiceController ser = new ServiceController();
+                ser.ServiceName = "mssqlserver";
+                if (ser.Status != ServiceControllerStatus.Running)
+                {
+                    calisti++;
+                    string path = "SqlStopStart.bat";
+                    if (!File.Exists(path))
+                    {
+                        File.WriteAllText("SqlStopStart.bat", "net start mssqlserver");
+                    }
+                    Process p = new Process();
+                    ProcessStartInfo psi = new ProcessStartInfo(path);
+                    psi.WindowStyle = ProcessWindowStyle.Hidden;
+                    psi.Verb = "runas";
+                    p.StartInfo = psi;
+                    p.Start();
+
+                    label2.Text = calisti.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                File.WriteAllText("HATA.txt", ""+ex.Message);
+            }
+           
+        }
+
+        public static int calisti = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            calisti = 0;
+            label2.Text = calisti.ToString();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = kapat;
+            this.Hide();
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    if (this.Visible == true)
+                    {
+                        this.Hide();
+                    }
+                    else
+                    {
+                        this.Show();
+                        this.Activate();
+                    }
+                }
+                else if (e.Button == MouseButtons.Right)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            notifyIcon1.ShowBalloonTip(1, "SQL R Otomatik Servis Açıldı", "SQL Servis", ToolTipIcon.Info);
+            this.Hide();
+        }
+
+        bool kapat = true;
+        private void btnKapat_Click(object sender, EventArgs e)
+        {
+            kapat = false;
+            this.Close();
+        }
+    }
+}
